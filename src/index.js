5c64332ef361af5cf2586f0194edda500fd0f029
devFerzen <alanferzen.ss@gmail.com>
2022-10-07 16:32:18 -0500

    working state localhost

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import express from "express"; //express-graphql or express
 import jwt from "jsonwebtoken";
 import jwt_decode from "jwt-decode";
 
-import graphqlSchema from "./graphql/schema.js";
+import graphqlSchema from "./graphql/schema";
 /* AFSS - Investigación pendiente
   -> Las dependencies de arriba general un warning 61608 junto con
       graphql-tools se analizará después al corto, quizás mediano plazo
@@ -12,15 +12,15 @@ import graphqlSchema from "./graphql/schema.js";
 */
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import permissions from "./graphql/permisos.js";
+import permissions from "./graphql/permisos";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import multer from "multer";
 import path from "path";
 import cloudinary from "cloudinary";
 
-import creacionToken from "./utilities/autorizacionToken.js";
-import Models from "./graphql/models/index.js";
+import creacionToken from "./utilities/autorizacionToken";
+import Models from "./graphql/models/index";
 import serveStatic from "serve-static";
 
 //Conexión MongoDb
