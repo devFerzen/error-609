de779556af1cad69de94bbd922ca13728797b087
devFerzen <alanferzen.ss@gmail.com>
2022-10-07 16:52:23 -0500

    deleting graphql queries  mjs ext

--- src/graphql/schema.mjs
+++ src/graphql/schema.mjs
@@ -3,31 +3,31 @@ import { merge } from 'lodash';
 
 import {
   typeDef as TiposGlobalesBase
-} from './queries/tipos-globales-base.mjs';
+} from './queries/tipos-globales-base.js';
 
 import {
   typeDef as TiposGlobalesSubbase
-} from './queries/tipos-globales-subBase.mjs';
+} from './queries/tipos-globales-subBase.js';
 
 import {
   typeDef as AnuncioQueries,
   resolvers as AnuncioResolvers
-} from './queries/anuncio-queries.mjs';
+} from './queries/anuncio-queries.js';
 
 import {
   typeDef as GeneralQueries,
   resolvers as GeneralResolvers
-} from './queries/general-queries.mjs';
+} from './queries/general-queries.js';
 
 import {
   typeDef as PaqueteQueries,
   resolvers as PaqueteResolvers
-} from './queries/paquetes-queries.mjs';
+} from './queries/paquetes-queries.js';
 
 import {
   typeDef as UsuarioQueries,
   resolvers as UsuarioResolvers
-} from './queries/usuario-queries.mjs';
+} from './queries/usuario-queries.js';
 
 const Query = `
   type Query {
