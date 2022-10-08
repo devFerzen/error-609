856981b993202f5f6a189767b97ab205d3e054ab
devFerzen <alanferzen.ss@gmail.com>
2022-10-08 16:47:10 -0500

    moving graphql up, with modifications3

--- graphql/queries/anuncio-queries.js
+++ graphql/queries/anuncio-queries.js
@@ -1,12 +1,12 @@
 import gql from "graphql-tag";
 
-import QueryAnuncio from "../src/utilities/queryAnuncio.js";
-import UsuarioClass from "../src/utilities/Usuario.js";
+import QueryAnuncio from "../../src/utilities/queryAnuncio.js";
+import UsuarioClass from "../../src/utilities/Usuario.js";
 import {
   crearBitacoraCreaciones,
   crearVerificacionAnuncio,
   crearBitacoraBusquedas,
-} from "../src/utilities/bitacoras.js";
+} from "../../src/utilities/bitacoras.js";
 import mongoose from 'mongoose';
 import path from "path";
 import fs from "fs";
