6a7c895a632a867cb189366e6d7909c090c78cc9
devFerzen <alanferzen.ss@gmail.com>
2022-10-11 11:42:00 -0500

    models in mjs

--- graphql/models/index.js
+++ graphql/models/index.js
@@ -1,10 +1,10 @@
-import Usuario from './usuario.cjs';
-import Anuncio from './anuncio.cjs';
-import BitacoraCreaciones from './bitacoraCreaciones.cjs';
-import AnunciosEnVerificacion from './anunciosEnVerificacion.cjs';
-import Paquete from './paquete.cjs';
-import Correo from './correo.cjs';
-import DdlGeneral from './ddlsGenerales.cjs';
+import Usuario from './usuario.mjs';
+import Anuncio from './anuncio.mjs';
+import BitacoraCreaciones from './bitacoraCreaciones.mjs';
+import AnunciosEnVerificacion from './anunciosEnVerificacion.mjs';
+import Paquete from './paquete.mjs';
+import Correo from './correo.mjs';
+import DdlGeneral from './ddlsGenerales.mjs';
 
 export default {
     Usuario,
